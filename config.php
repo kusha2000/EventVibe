21d83f6ef46ce27addf18f44fb3af9e26833a3e7
kusha2000 <kushanandarawewa1@gmail.com>
2023-04-02 19:01:21 +0530

    New database Connected

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 //$conn = mysqli_connect('localhost','root','','eventvibe') or die('connection failed');
 //$conn = mysqli_connect('jdbc:mysql://sql12.freesqldatabase.com:3306/sql12607400','sql12607400','QW8AK47F1q','eventvibe');
-$conn = mysqli_connect('sql12.freesqldatabase.com','sql12607400','QW8AK47F1q','sql12607400');
+$conn = mysqli_connect('sql12.freesqldatabase.com','sql12610108','tzklByQlqj','sql12610108');
 //$conn = mysqli_connect('mysql: dbname=id20491307_eventvibe; host=localhost','id20491307_eventvibeweb','QW8AK47F1q','4Q-/<g=5GQRwknG8');
 
 /*try{
