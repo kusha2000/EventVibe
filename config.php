709387e60c19eef3222dfc02b9410845930d5c54
kusha2000 <kushanandarawewa1@gmail.com>
2023-04-08 22:42:15 +0530

    Connect to new Database Server

--- config.php
+++ config.php
@@ -2,8 +2,10 @@
 
 //$conn = mysqli_connect('localhost','root','','eventvibe') or die('connection failed');
 //$conn = mysqli_connect('jdbc:mysql://sql12.freesqldatabase.com:3306/sql12607400','sql12607400','QW8AK47F1q','eventvibe');
-$conn = mysqli_connect('sql12.freesqldatabase.com','sql12610108','tzklByQlqj','sql12610108');
-//$conn = mysqli_connect('mysql: dbname=id20491307_eventvibe; host=localhost','id20491307_eventvibeweb','QW8AK47F1q','4Q-/<g=5GQRwknG8');
+//$conn = mysqli_connect('sql12.freesqldatabase.com','sql12610108','tzklByQlqj','sql12610108');
+//$conn = mysqli_connect('mysql-120194-0.cloudclusters.net','admin','vDmxpQUR','eventVibe');
+$conn = new mysqli('mysql-120194-0.cloudclusters.net','admin','vDmxpQUR','eventVibe',10090);
+
 
 /*try{
     //$dsn="jdbc:mysql://sql6.freesqldatabase.com:3306/sql12607400";
